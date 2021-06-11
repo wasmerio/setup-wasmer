0e837f3cb973084986786021535be4dff5a505db
Wakeful-Cloud <wakefulcloud@gmail.com>
2021-06-11 04:17:41 -0600

    Added existing files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Setup Wasmer Action
+[GitHub action](https://github.com/features/actions) for setting up [Wasmer](https://wasmer.io)
+
+## Usage
+```yaml
+name: Setup Wasmer
+uses: cloud-cnc/setup-wasmer@v1
+```
+
+## I/O
+There is no input nor output from this action
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+/**
+ * @fileoverview Setup Wasmer
+ */
+
+//Imports
+import got from 'got';
+import {addPath, debug, endGroup, exportVariable, info, setFailed, startGroup} from '@actions/core';
+import {createWriteStream} from 'fs';
+import {exec} from '@actions/exec';
+import {homedir} from 'os';
+import {join} from 'path';
+import {path} from 'temp';
+import {promisify} from 'util';
+import {pipeline as pipelineCB} from 'stream';
+
+//Promisify pipes
+const pipeline = promisify(pipelineCB);
+
+const main = async () =>
+{
+  //Get if the host is Windows or not
+  const isWindows = process.platform == 'win32';
+
+  //Create a temporary file to store the installer
+  const tmp = path({
+    suffix: isWindows ? '.ps1' : '.sh'
+  });
+
+  debug(`Using ${tmp} as temporary path.`);
+
+  //Download the latest installer
+  startGroup('Download the installer.');
+  const progressHandler = (data: {transferred: number, total: number, percent: number}) =>
+  {
+    //Round
+    const transferred = Math.round(data.transferred / 1000);
+    const total = Math.round(data.total / 1000);
+    const percent = Math.round(data.percent * 100);
+
+    //Log
+    info(`Downloaded ${transferred}K out of ${total}K (${percent}%)`);
+  };
+  await pipeline(
+    got.stream(isWindows ? 'https://win.wasmer.io' : 'https://get.wasmer.io').on('downloadProgress', progressHandler),
+    createWriteStream(tmp, {
+      mode: 0o655
+    })
+  );
+  endGroup();
+
+  info('Downloaded installer.');
+
+  //Execute the installer
+  startGroup('Execute the installer.');
+  await exec('stat', ['-c', '"%s"', tmp]);
+  await exec('cat', [tmp]);
+  const exitCode = await exec(isWindows ? 'pwsh' : 'sh', [
+    tmp
+  ]);
+
+  if (exitCode != 0)
+  {
+    setFailed(`Installer failed! (Exit code ${exitCode})`);
+  }
+  endGroup();
+
+  info(`Executed installer. (Exit code ${exitCode})`);
+
+  //Update environment variables
+  if (!isWindows)
+  {
+    const wasmerDir = join(homedir(), '.wasmer');
+    debug(`Using ${wasmerDir} as Wasmer directory.`);
+
+    exportVariable('WASMER_DIR', wasmerDir);
+    exportVariable('WASMER_CACHE_DIR', join(wasmerDir, 'cache'));
+    addPath(join(wasmerDir, 'bin'));
+    addPath(join(wasmerDir, 'globals/wapm_packages/.bin'));
+  }
+
+  info('Updated environment variables.');
+};
+
+main().catch(error =>
+{
+  throw error;
+});
